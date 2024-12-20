6c1ad10dbdd1983ebd31319795000951947e8d6c
Ubuntu <alceballosa@alberto-nvidia-4xA100-vm1.nooi5hcj5piejb40vg5vr2dqvg.wawx.internal.cloudapp.net>
2024-12-20 17:38:18 +0000

    Add vivit model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,5 +2,5 @@
 # from .full_me_net import MESparseCPM
 # from .semi_sp_me_net import MESemiSparseCPM
 from .deformable.def_parq_rec import PARQ_Deformable_R
-
+from .deformable.vivit_parq_rec import PARQ_ViViT
 
