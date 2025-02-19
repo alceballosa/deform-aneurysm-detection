e975659a8dcf7da2279313d49a547461aee628ca
Alberto Mario Ceballos Arroyo <alceballosa@gmail.com>
2025-02-19 22:12:41 +0000

    fix distance maps

--- src/preprocess/compute_distance_maps.py
+++ src/preprocess/compute_distance_maps.py
@@ -15,6 +15,30 @@ import edt
 import numpy as np
 import SimpleITK as sitk
 import tqdm
+from joblib import Parallel, delayed
+
+
+def compute_distance_map(file, target_dir, compress):
+    scan_seriesuid = file.split("/")[-1]
+    save_file_path = os.path.join(target_dir, scan_seriesuid)
+    # check if exists
+    if os.path.exists(save_file_path):
+        return
+    im_header = sitk.ReadImage(file)
+    im = sitk.GetArrayFromImage(im_header)
+    im = im == 1 # only arteries
+    im = im.astype(int)
+    if len(np.unique(im)) < 2:
+        print("Potential error on ", file)
+    im_dist = edt.sdf(im, black_border=False)#, parallel=1)
+    if compress == 1:
+        threshold = 128
+        im_dist[im_dist < -threshold] = -threshold
+    # print(im_dist.shape)
+    im_dist_header = sitk.GetImageFromArray(im_dist)
+    im_dist_header.CopyInformation(im_header)
+    
+    sitk.WriteImage(im_dist_header, save_file_path)
 
 if __name__ == "__main__":
     vessel_dir = sys.argv[1]
@@ -23,20 +47,19 @@ if __name__ == "__main__":
         threads = int(sys.argv[3])
     except IndexError:
         threads = 8
+    compress = sys.argv[4]
+    try:
+        compress = int(compress)
+    except:
+        compress = 0
+
     os.makedirs(target_dir, exist_ok=True)
     vessel_files = sorted(list(glob(f"{vessel_dir}/*.nii.gz")))
-    for file in tqdm.tqdm(vessel_files):
-        scan_seriesuid = file.split("/")[-1]
-        save_file_path = os.path.join(target_dir, scan_seriesuid)
-        # check if exists
-        if os.path.exists(save_file_path):
-            continue
-        im_header = sitk.ReadImage(file)
-        im = sitk.GetArrayFromImage(im_header)
-        # im = (im > threshold).astype(np.uint8)
-        im_dist = edt.sdf(im, black_border=False, parallel=threads)
-        im_dist_header = sitk.GetImageFromArray(im_dist)
-        im_dist_header.CopyInformation(im_header)
-        
-        sitk.WriteImage(im_dist_header, save_file_path)
+    executor = Parallel(
+        n_jobs=threads, backend="multiprocessing", prefer="processes", verbose=1
+    )
+    do = delayed(compute_distance_map)
+    tasks = (do(im_f, target_dir, compress) for im_f in vessel_files)
+    executor(tasks)
+
     print("All distance maps computed!")
