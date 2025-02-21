d423ed7e5fea9f5090bc5a35225418b587fc7823
Millicent Li <mllcntl@gmail.com>
2025-02-21 16:38:24 +0000

    Remove spatialcls token

--- src/models/deformable/vivit_backbone_1l.py
+++ src/models/deformable/vivit_backbone_1l.py
@@ -19,7 +19,7 @@ def build_backbone(cfg):
         frames=cfg.DATA.PATCH_SIZE[0],  # number of frames
         image_patch_size=4,  # image patch size
         frame_patch_size=4,  # frame patch size
-        num_classes=2,
+        num_classes=1,
         channels=cfg.DATA.N_CHANNELS,
         dim=cfg.MODEL.D_MODEL,
         spatial_depth=3,  # depth of the spatial transformer
@@ -217,11 +217,11 @@ class ViT(Base_Backbone):
         )
         self.dropout = nn.Dropout(emb_dropout)
 
-        self.spatial_cls_token = (
-            nn.Parameter(torch.randn(1, 1, dim))
-            if not self.global_average_pool
-            else None
-        )
+        # self.spatial_cls_token = (
+        #     nn.Parameter(torch.randn(1, 1, dim))
+        #     if not self.global_average_pool
+        #     else None
+        # )
 
         if variant == "factorized_encoder":
             self.temporal_cls_token = (
@@ -255,11 +255,11 @@ class ViT(Base_Backbone):
 
         x = x + self.pos_embedding[:, :f, :n]
 
-        if exists(self.spatial_cls_token):
-            spatial_cls_tokens = repeat(
-                self.spatial_cls_token, "1 1 d -> b f 1 d", b=b, f=f
-            )
-            x = torch.cat((spatial_cls_tokens, x), dim=2)
+        # if exists(self.spatial_cls_token):
+        #     spatial_cls_tokens = repeat(
+        #         self.spatial_cls_token, "1 1 d -> b f 1 d", b=b, f=f
+        #     )
+        #     x = torch.cat((spatial_cls_tokens, x), dim=2)
 
         x = self.dropout(x)
 
@@ -299,7 +299,8 @@ class ViT(Base_Backbone):
         elif self.variant == "factorized_self_attention":
             x = self.factorized_transformer(x)
             # remove temporal cls token
-            x = x[:, :, 1:, :]
+            #print(x.shape)
+            #x = x[:, :, 1:, :]
             spatial_dim = int(x.shape[-2] ** 0.5)
             x = rearrange(x, "b z (x y) c -> b z x y c", x=spatial_dim, y=spatial_dim)
             # make channels the second dim 
