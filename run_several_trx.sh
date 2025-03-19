2134b8362b93f92ab56e606860798e893d46bdea
Alberto Mario Ceballos Arroyo <alceballosa@gmail.com>
2025-03-19 15:12:52 +0000

    New configs

--- run_several_trx.sh
+++ run_several_trx.sh
@@ -4,9 +4,16 @@
 export id_gpu=0,1,2,3
 export family="trx"
 
-export model="trx_fa_input_edt"
+export model="cnn_input_edt_cvs"
 
 
+./src/run_inference_test.sh $family $model "final" 0.8 $id_gpu
+./src/run_inference_test.sh $family $model "0063999" 0.8 $id_gpu
+./src/run_inference_test.sh $family $model "0059999" 0.8 $id_gpu
+./src/run_inference_test.sh $family $model "0055999" 0.8 $id_gpu
+./src/run_inference_test.sh $family $model "0051999" 0.8 $id_gpu
+./src/run_inference_test.sh $family $model "0047999" 0.8 $id_gpu
+./src/run_inference_test.sh $family $model "0039999" 0.8 $id_gpu
 
 ./src/run_inference_cmha.sh $family $model "final" 0.8 $id_gpu
 ./src/run_inference_cmha.sh $family $model "0063999" 0.8 $id_gpu
@@ -16,17 +23,12 @@ export model="trx_fa_input_edt"
 ./src/run_inference_cmha.sh $family $model "0047999" 0.8 $id_gpu
 ./src/run_inference_cmha.sh $family $model "0039999" 0.8 $id_gpu
 
-./src/run_inference_external.sh $family $model "final" 0.8 $id_gpu
-./src/run_inference_external.sh $family $model "0063999" 0.8 $id_gpu
-./src/run_inference_external.sh $family $model "0059999" 0.8 $id_gpu
-./src/run_inference_external.sh $family $model "0055999" 0.8 $id_gpu
-./src/run_inference_external.sh $family $model "0051999" 0.8 $id_gpu
-./src/run_inference_external.sh $family $model "0047999" 0.8 $id_gpu
-./src/run_inference_external.sh $family $model "0039999" 0.8 $id_gpu
+./src/run_inference_ext.sh $family $model "final" 0.8 $id_gpu
+./src/run_inference_ext.sh $family $model "0063999" 0.8 $id_gpu
+./src/run_inference_ext.sh $family $model "0059999" 0.8 $id_gpu
+./src/run_inference_ext.sh $family $model "0055999" 0.8 $id_gpu
+./src/run_inference_ext.sh $family $model "0051999" 0.8 $id_gpu
+./src/run_inference_ext.sh $family $model "0047999" 0.8 $id_gpu
+./src/run_inference_ext.sh $family $model "0039999" 0.8 $id_gpu
 
-./src/run_inference_test.sh $family $model "0063999" 0.8 $id_gpu
-./src/run_inference_test.sh $family $model "0059999" 0.8 $id_gpu
-./src/run_inference_test.sh $family $model "0055999" 0.8 $id_gpu
-./src/run_inference_test.sh $family $model "0051999" 0.8 $id_gpu
-./src/run_inference_test.sh $family $model "0047999" 0.8 $id_gpu
-./src/run_inference_test.sh $family $model "0039999" 0.8 $id_gpu
+
--- src/run_inference_test.sh
+++ src/run_inference_test.sh
@@ -10,6 +10,6 @@ CUDA_VISIBLE_DEVICES=$5 python src/train_net.py\
     --dist-url "tcp://127.0.0.1:$ID_PORT"\
     --eval-only\
     MODEL.WEIGHTS $3\
-    DATA.DIR.VAL.SCAN_DIR "/data/aneurysm/cmha/crop_0.4"\
+    DATA.DIR.VAL.SCAN_DIR "/data/aneurysm/internal_test/crop_0.4"\
     DATA.DIR.VAL.ANNOTATION_FILE "./labels/gt/internal_test_crop_0.4.csv"\
-    DATA.DIR.VAL.VESSEL_DIR "/data/aneurysm/cmha/crop_0.4_vessel_edt_comp"
+    DATA.DIR.VAL.VESSEL_DIR "/data/aneurysm/internal_test/crop_0.4_vessel_edt_comp"
