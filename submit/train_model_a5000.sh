c57592db4d9ba5b80281a155b45cc1ec25cfa863
Alberto Mario Ceballos-Arroyo <alceballosa@gmail.com>
2026-02-16 08:41:32 -0500

    New vscond configs

--- submit/train_model_a5000.sh
+++ submit/train_model_a5000.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=jiang
 #SBATCH --time=150:00:00
 #SBATCH --mem=512
-#SBATCH --gres=gpu:a5000:8
+#SBATCH --gres=gpu:a5000:4
 #SBATCH --cpus-per-gpu=8
 #SBATCH --output=./logs/exec.%j.train.%x.out
 #SBATCH --error=./logs/exec.%j.train.%x.out
